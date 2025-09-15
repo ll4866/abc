600d8eea753b3f0b5673f6b2f552a41b7d6e5a28
ll4866 <ll4866@nyu.edu>
2025-09-15 16:25:12 +0800

    Capture Code

--- deviceOrientation/public/sketch.js
+++ deviceOrientation/public/sketch.js
@@ -1,5 +1,9 @@
 let alpha, beta, gamma = 0;
 let targetAlpha, targetBeta, targetGamma = 0;
+let captured = false;
+let buttonW = 100;
+let buttonH = 60;
+let score = 0;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
@@ -12,42 +16,31 @@ function setup() {
 function draw() {
   background(90, 200, 190);
   
+  // ----- Information ------
   noStroke();
-
-  // push();
-  //   translate(width/2, height/2);
-  //   rotate(radians(alpha))
-    
-  //   // black rectangle
-  //   fill(0);
-  //   rect(-100, -100, 200, 200);
-    
-  //   // red circle
-  //   fill(255, 0, 0);
-  //   circle(0, -100, 5)
-  // pop();
-
   fill(0);
   text("alpha: " + round(alpha), 10, 30);
   text("beta: " + round(beta), 10, 40);
   text("gamma: " + round(gamma), 10, 50);
 
+  // ------ Drawing the Target -----
   // Distance btw Phone and Target
-  const SCALE = 4; // pixels per degree
+  const SCALE = 4;
 
   let disAlpha = alpha - targetAlpha;
-  /* convert (0,360) to (-180, 180) for better calculation: https://www.orbiter-forum.com/threads/looking-for-formula-to-convert-0-to-360-to-180-to-180.20767/post-320248
+  /* convert (0,360) to (-180, 180:
+   https://www.orbiter-forum.com/threads/looking-for-formula-to-convert-0-to-360-to-180-to-180.20767/post-320248
   */
   disAlpha = ((disAlpha + 180) % 360) - 180;    
   let distBeta  = beta - targetBeta;
 
-  //xy-coordinate = Center + diff (up, down, right left)
-  let drawX = width/2  + disAlpha * SCALE;
-  let drawY = height/2 + distBeta * SCALE;
-
-  // Calculate distance it is from the target
+  // XY Distance
   let d = sqrt(disAlpha*disAlpha + distBeta*distBeta );
   let withinZone = 60;
+  
+  // XY-coordinate = Center + diff (up, down, right left)
+  let drawX = width/2  + disAlpha * SCALE;
+  let drawY = height/2 + distBeta * SCALE;  
 
   // Color Change if apprach zone range
   if (d < withinZone/3){
@@ -58,18 +51,57 @@ function draw() {
   circle(drawX, drawY, 30);
 
   // Target drawing
-  stroke(255,0,0);
-  strokeWeight(1);
-  fill(0,0,0,10);
-  circle(width/2, height/2, 120);  
-  fill(255,0,0);
-  circle(width/2, height/2, 5);  
+  if(d < 5 && captured == true){
+    score++;
+    captured = false;
+    newTarget();
+  }
+  push();
+    translate(width/2, height/2);
+    stroke(255,0,0);
+    strokeWeight(1);
+    fill(0,0,0,10);
+    circle(0,0, 120);  
+    fill(255,0,0);
+    circle(0,0, 5);  
+    noStroke();
+    fill(0);
+    textAlign(CENTER, CENTER);
+    text("Score:" + score, 0, -80);
+  pop();
+
+  // Capture button
+  push();
+    translate(width - 80, height - 80);
+    noStroke();
+    fill(0,0,0, 220);
+    rectMode(CENTER);
+    rect(0, 0, buttonW, buttonH, 8);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(16);
+    text("Capture", 0, 0);
+  pop();
+}
+
+function newTarget() {
+  targetAlpha = random(360);   // 0-360
+  targetBeta  = random(-180, 180);
+  targetGamma = random(-90, 90);
 }
 
 // P5 touch events: https://p5js.org/reference/#Touch 
 
 function touchStarted() {
-  console.log(touches);
+  let x = width - 80 - buttonW/2;
+  let y = height - 80 - buttonH/2;
+
+  for (let t of touches) {
+    if (t.x > x && t.x < x + buttonW && t.y > y && t.y < y + buttonH) {
+      captured = true;
+      return false; 
+    }
+  }
 }
 
 function touchMoved() {
@@ -90,5 +122,4 @@ function handleOrientation(eventData){
   alpha = eventData.alpha;
   beta = eventData.beta;
   gamma = eventData.gamma;
-    
 }
